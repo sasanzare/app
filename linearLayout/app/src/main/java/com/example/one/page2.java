59ecf4f888fd73083273b687a1cb3ffb84c7f3c9
sasan zare <sasanzare75@gmail.com>
2021-03-25 19:38:39 +0430

    feat: Initial commit

---
+++ linearLayout/app/src/main/java/com/example/one/page2.java
@@ -0,0 +1,14 @@
+package com.example.one;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page2 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_page2);
+  }
+}
